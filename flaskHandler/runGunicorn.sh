48a569c0c5fcae59df9f2a0a2bbdddac6d08c89d
Wey GU <littlewey@gmail.com>
2017-09-03 01:15:13 -0400

    gunicorn timeout

--- flaskHandler/runGunicorn.sh
+++ flaskHandler/runGunicorn.sh
@@ -1,3 +1,3 @@
-gunicorn -w 4 -b 127.0.0.1:8093 wsgi --reload &
+gunicorn -w 4 -b 127.0.0.1:8093 wsgi --reload --timeout 40&
 service nginx restart
 
--- flaskHandler/wsgi.py
+++ flaskHandler/wsgi.py
@@ -2,4 +2,4 @@
 from app import app
 application = app
 if __name__ == '__main__':
-    application.run()
+    application.run(debug=True)
